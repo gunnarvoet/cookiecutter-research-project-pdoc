2de08fd05b200516c680503a2f2f9e7dd0737763
Gunnar Voet <gvoet@ucsd.edu>
2021-02-25 11:18:37 -0800

    add autoimports to init

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
@@ -3,3 +3,6 @@
 __author__ = """{{ cookiecutter.full_name }}"""
 __email__ = '{{ cookiecutter.email }}'
 __version__ = '{{ cookiecutter.version }}'
+
+__all__ = ["io", "{{cookiecutter.project_slug}}"]
+from . import io, {{cookiecutter.project_slug}}
