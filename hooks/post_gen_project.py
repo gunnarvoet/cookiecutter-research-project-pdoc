d9094cc76c28a0bab64233b3db4a3abda6f3c271
Gunnar Voet <gvoet@ucsd.edu>
2023-12-15 10:13:13 -0800

    remove cli

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -40,10 +40,6 @@ def add_pdoc_theme_submodule():
 
 if __name__ == '__main__':
 
-    if 'no' in '{{ cookiecutter.command_line_interface|lower }}':
-        cli_file = os.path.join('{{ cookiecutter.project_slug }}', 'cli.py')
-        remove_file(cli_file)
-
     if 'Not open source' == '{{ cookiecutter.open_source_license }}':
         remove_file('LICENSE')
 
